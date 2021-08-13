c44127825202d6c104a140103e3beac2b66d367d
EnzoSalvadori <63012963+EnzoSalvadori@users.noreply.github.com>
2021-08-13 12:19:59 -0300

    ajuste na fila

--- milho/management/commands/fila.py
+++ milho/management/commands/fila.py
@@ -18,7 +18,7 @@ class Command(BaseCommand):
 				for i in range(len(imagem)):
 					path_img = imagem[i].imagemOrg.path 
 					usuario = User.objects.filter(id = imagem[i].fk_user.id)
-					t1 = Thread(target=processa,args=[path_img,imagem[i].id,usuario[0].email])
+					t1 = Thread(target=processa,args=[path_img,imagem[i].id,usuario[0]])
 					t1.start()
 					Imagem.objects.filter(processada = "1", fk_user = imagem[i].fk_user).update(fila = False)
 					if (psutil.virtual_memory()[2] >= 80): #se a memoria do servidor chegar em 80% parar de enviar novos processos
